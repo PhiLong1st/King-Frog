339c286829b46f4886ed680b9766da7d8382ce2c
PhiLong1st <philongnguyen1704@gmail.com>
2026-06-21 11:35:15 +0700

    feat: add AnimationUtils with FadeAnimation coroutine

---
+++ Assets/Scripts/Utils/AnimationUtils.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public static class AnimationUtils
+{
+  public static IEnumerator FadeAnimation(float startAlpha, float targetAlpha, float duration, Action<float> onUpdate)
+  {
+    float elapsedTime = 0f;
+    while (elapsedTime < duration)
+    {
+      elapsedTime += Time.deltaTime;
+
+      float currentAlpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / duration);
+      onUpdate?.Invoke(currentAlpha);
+
+      yield return null;
+    }
+
+    onUpdate?.Invoke(targetAlpha);
+  }
+}
